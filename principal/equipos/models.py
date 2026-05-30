79f4e6d2d3ac3fb5e531ed4bb483af2fd41f3f28
juniorsmith21 <ordonezjun0@gmail.com>
2026-05-30 11:10:25 -0500

    agregamos requiriments

--- principal/equipos/models.py
+++ principal/equipos/models.py
@@ -1,28 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Equipo(models.Model):
-
-    TIPO_CHOICES=[
-        ('electrico', 'Electrico'),
-        ('mecanico', 'Mecanico'),
-        ('hidraulico', 'Hidraulico'),
-        ('neumatico', 'Neumatico'),
-        ('electronico', 'Electronico'),
-
-
-    ]
-
-    nombre                  =models.CharField(max_length=150)
-    codigo                  =models.CharField(max_length=50, unique=True)
-    tipo                    =models.CharField(max_length=50, choices=TIPO_CHOICES)
-    descripcion             =models.TextField(blank=True)
-    ubicacion               =models.CharField(max_length=200)
-    fecha_de_anquisicion    =models.DateField()
-    activo                  =models.BooleanField(default=True)
-
-
-    def __str__(self):
-        return f'[{self.codigo}] {self.nombre}'
-    class Meta:
-        ordering=['nombre']
+ 
